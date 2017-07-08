7c6196496ce50625cf8eb2a0f8e88c378f260e7b
MineShadow <shad-karagodin@ya.ru>
2017-07-08 23:55:46 +0300

    First attempt

--- src/main/java/com/minecolonies/coremod/blocks/BlockDecorative.java
+++ src/main/java/com/minecolonies/coremod/blocks/BlockDecorative.java
@@ -1,154 +1,83 @@
 package com.minecolonies.coremod.blocks;
 
-import com.minecolonies.api.util.constant.Constants;
-import com.minecolonies.coremod.creativetab.ModCreativeTabs;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IStringSerializable;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
 
-public class BlockDecorative extends Block
-{
-    public static final PropertyEnum<BlockDecorative.EnumType> VARIANT = PropertyEnum.<BlockDecorative.EnumType>create("variant", BlockDecorative.EnumType.class);
-
-    /**
-     * The hardness this block has.
-     */
-    private static final float BLOCK_HARDNESS = 0.0F;
-
-    /**
-     * This blocks name.
-     */
-    private static final String BLOCK_NAME = "blockDecorative";
-
-    /**
-     * The resistance this block has.
-     */
-    private static final float RESISTANCE = 1F;
-
-
-    private void initBlock()
-    {
-        setRegistryName(BLOCK_NAME);
-        setUnlocalizedName(String.format("%s.%s", Constants.MOD_ID.toLowerCase(), BLOCK_NAME));
-        setCreativeTab(ModCreativeTabs.MINECOLONIES);
-        GameRegistry.register((new com.minecolonies.coremod.blocks.BlockDecorative()).setRegistryName(this.getRegistryName()));
-        setHardness(BLOCK_HARDNESS);
-        setResistance(RESISTANCE);
+public class BlockDecorative extends Block {
+    public BlockDecorative(String unlocalizedName, Material material, float hardness, float resistance) {
+        super(material);
+        this.setUnlocalizedName(unlocalizedName);
+        this.setCreativeTab(CreativeTabs.DECORATIONS);
+        this.setHardness(hardness);
+        this.setResistance(resistance);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(TYPE, EnumType.Crossed));
     }
 
-    public BlockDecorative()
-    {
-        super(Material.WOOD);
-        initBlock();
-    }
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public boolean isOpaqueCube(final IBlockState state)
-    {
-        return true;
-    }
+    public static final PropertyEnum<com.minecolonies.coremod.blocks.EnumType> TYPE = PropertyEnum.create("type", BlockDecorative.EnumType.class);
 
-    /**
-     * Gets the metadata of the item this Block can drop. This method is called when the block gets destroyed. It
-     * returns the metadata of the dropped item based on the old metadata of the block.
-     */
-    public int damageDropped(IBlockState state)
-    {
-        return ((BlockDecorative.EnumType)state.getValue(VARIANT)).getMetadata();
-    }
+    public enum EnumType implements com.minecolonies.coremod.blocks.EnumType {
+        Crossed(0, "crossed"),
+        Plain(1, "plain");
 
-    /**
-     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
-     */
-    @SideOnly(Side.CLIENT)
-    public void getSubBlocks(Item itemIn, ModCreativeTabs tab, List<ItemStack> list)
-    {
-        for (com.minecolonies.coremod.blocks.BlockDecorative.EnumType blockdecorative$enumtype : com.minecolonies.coremod.blocks.BlockDecorative.EnumType.values())
-        {
-            list.add(new ItemStack(itemIn, 1, blockdecorative$enumtype.getMetadata()));
-        }
-    }
+        private int ID;
+        private String name;
 
-    public enum EnumType implements IStringSerializable
-    {
-        TIMBER(0, "Timber", MapColor.WOOD),
-        CROSSED(1, "Crossed", MapColor.OBSIDIAN);
-
-        private static final com.minecolonies.coremod.blocks.BlockDecorative.EnumType[] META_LOOKUP = new com.minecolonies.coremod.blocks.BlockDecorative.EnumType[values().length];
-        private final int meta;
-        private final String name;
-        private final String unlocalizedName;
-        /** The color that represents this entry on a map. */
-        private final MapColor mapColor;
-
-        private EnumType(int metaIn, String nameIn, MapColor mapColorIn)
-        {
-            this(metaIn, nameIn, nameIn, mapColorIn);
+        private EnumType(int ID, String name) {
+            this.ID = ID;
+            this.name = name;
         }
 
-        private EnumType(int metaIn, String nameIn, String unlocalizedNameIn, MapColor mapColorIn)
-        {
-            this.meta = metaIn;
-            this.name = nameIn;
-            this.unlocalizedName = unlocalizedNameIn;
-            this.mapColor = mapColorIn;
+        @Override
+        public String getName() {
+            return name;
         }
 
-        public int getMetadata()
-        {
-            return this.meta;
+        @Override
+        public int getID() {
+            return ID;
         }
 
-        /**
-         * The color which represents this entry on a map.
-         */
-        public MapColor getMapColor()
-        {
-            return this.mapColor;
+        @Override
+        public String toString() {
+            return getName();
         }
 
-        public String toString()
-        {
-            return this.name;
+        @Override
+        protected BlockState createBlockState() {
+            return new BlockState(this, new IProperty[]{TYPE});
         }
 
-        public static com.minecolonies.coremod.blocks.BlockDecorative.EnumType byMetadata(int meta)
-        {
-            if (meta < 0 || meta >= META_LOOKUP.length)
-            {
-                meta = 0;
-            }
-
-            return META_LOOKUP[meta];
+        @Override
+        public IBlockState getStateFromMeta(int meta) {
+            return getDefaultState().withProperty(TYPE, meta == 0 ? EnumType.WHITE : EnumType.BLACK);
         }
 
-        public String getName()
-        {
-            return this.name;
+        @Override
+        public int getMetaFromState(IBlockState state) {
+            com.minecolonies.coremod.blocks.EnumType type = state.getValue(TYPE);
+            return type.getID();
         }
 
-        public String getUnlocalizedName()
-        {
-            return this.unlocalizedName;
+        @Override
+        public int damageDropped(IBlockState state) {
+            return getMetaFromState(state);
         }
 
-        static
-        {
-            for (com.minecolonies.coremod.blocks.BlockDecorative.EnumType blockdecorative$enumtype : values())
-            {
-                META_LOOKUP[blockdecorative$enumtype.getMetadata()] = blockdecorative$enumtype;
-            }
+        @Override
+        public void getSubBlocks(Item itemIn, CreativeTabs tab, List list) {
+            list.add(new ItemStack(itemIn, 1, 0)); //Meta 0
+            list.add(new ItemStack(itemIn, 1, 1)); //Meta 1
         }
     }
 }
+
+
